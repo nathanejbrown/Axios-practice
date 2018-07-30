56393c1d2a719e8ce357a7994e093b750c33b1fb
Heathcliff <dennis.nathan@gmail.com>
2018-07-30 17:17:46 -0600

    Uploading project

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(config => {
+  console.log('1', config);
+  return config;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(config => {
+  console.log(config);
+  return config;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+})
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
