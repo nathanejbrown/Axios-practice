f5bd12e3c1a3f566fbf73003fe32d308305428db
Heathcliff <dennis.nathan@gmail.com>
2018-08-01 12:36:27 -0600

    All prior features of app are now working, but with routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 import './Blog.css';
 
@@ -17,12 +18,19 @@ class Blog extends Component {
               <header>
                 <nav>
                   <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to={{
+                    <li><NavLink
+                      //the active params only work with NavLink, not Link.
+                      exact
+                      to="/"
+                      activeClassName="my-active"
+                      activeStyle={{
+                        color: '#fa923f'
+                      }}>Home</NavLink></li>
+                    <li><NavLink to={{
                       pathname: '/new-post',
                       hash: '#submit',
                       search: '?quick-submit=true'
-                    }}>New Post</Link></li>
+                    }}>New Post</NavLink></li>
                   </ul>
                 </nav>
               </header>
@@ -30,6 +38,7 @@ class Blog extends Component {
               <Route path="/" exact render={() => <h1>Home</h1>} /> */}
               <Route path="/" exact component={Posts} />
               <Route path="/new-post" component={NewPost} />
+              <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
